b0bf927727525b6d399586b57866486e23d5155f
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-07-08 23:59:09 +0300

    update changes

--- quiz.js
+++ quiz.js
@@ -1,4 +1,25 @@
+function checkAnswer() {
+    //Function body
+let correctAnswer = 4;
+//use document.querySelector to select the input elements
+let selectedOption = document.querySelector('input[name="quiz"] :checked');
+if (selectedOption) {
+    let userAnswer = selectedOption.value;
+    console.log('user selected:',userAnswer);
+    if (userAnswer === correctAnswer){
+        console.log("corect! Well done.");
+    }
+    else {
+        console.log("That's incorrect. Try again!");
+    } 
+}
+    else {
+        console.log("Please select an answer.");
+    }
+}
 
+let submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
 
 
 
